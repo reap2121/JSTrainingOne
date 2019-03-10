6943b1ab12aed3b816faf8a798cfabf9254b8f1b
candyzje <reap2121@gmail.com>
2019-03-10 16:51:47 +0100

    Added a button

--- JSTraining/index.js
+++ JSTraining/index.js
@@ -1,11 +1,13 @@
 const express = require('express')
 var path = require('path')
+const pug = require('pug')
 const app = express()
 const port = 3000
 
 app.set('view engine', 'pug');
 app.set('views', path.join(__dirname, '../views'));
 app.use(express.static(__dirname + '/../resources'));
+
 app.get('/', (req, res) => res.render('page', {
     title: 'anyád'
 }))
