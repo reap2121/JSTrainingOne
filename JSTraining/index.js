fb112aec56013648cbb32a03ccc3672eecf1acf3
candyzje <reap2121@gmail.com>
2019-03-10 16:05:55 +0100

    Added css

--- JSTraining/index.js
+++ JSTraining/index.js
@@ -1,8 +1,14 @@
 const express = require('express')
+var path = require('path')
 const app = express()
 const port = 3000
 
-app.get('/', (req, res) => res.send('Hello World!'))
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, '../views'));
+app.use(express.static(__dirname + '/../resources'));
+app.get('/', (req, res) => res.render('page', {
+    title: 'anyád'
+}))
 
 const server = app.listen(port, () => {
     console.log(`Express running → PORT ${server.address().port}`)
